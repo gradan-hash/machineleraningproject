cd58ffebb22728d78ce8cce4f6bed67afb083e6f
gradan-hash <mutukunyaa@gmail.com>
2023-08-09 14:17:16 +0300

    exception handling

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,10 @@
 import sys
+import logging
 
 
-def error_message_details(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
+def error_message_details(error, error_detail):
+    # Use sys.exc_info() instead of error_detail.exc_info()
+    _, _, exc_tb = sys.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error Occurred on Python Script [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error))
@@ -10,19 +12,19 @@ def error_message_details(error, error_detail: sys):
 
 
 class CustomException(Exception):
-
-    def __init__(self, error_message, error, error_detail: sys):
+    def __init__(self, error, error_detail):
+        error_message = error_message_details(error, error_detail)
         super().__init__(error_message)
-        self.error_message = error_message_details(
-            error, error_detail)
+        self.error_message = error_message  # Store the error message details
 
     def __str__(self):
         return self.error_message
 
 
-# try:
-#     # Some code that might raise an exception
-#     raise CustomException("Custom error message",
-#                           ValueError("Something went wrong"))
-# except CustomException as ce:
-#     print(ce.error_message)
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        # Pass sys.exc_info() instead of sys
+        raise CustomException(e, sys.exc_info())
