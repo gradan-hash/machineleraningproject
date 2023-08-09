aa7addc62f077caa5ee0960a83d26adf7fb3fbcd
gradan-hash <mutukunyaa@gmail.com>
2023-08-09 17:30:37 +0300

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_details(error, error_detail):
@@ -19,3 +19,12 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 0
+#     except Exception as e:
+#         logging.info("Divide by zero error")
+#         # Pass sys.exc_info() instead of sys
+#         raise CustomException(e, sys.exc_info())
