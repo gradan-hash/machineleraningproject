ad1aadab1b6f2ee7725aee0913bc8b3e2d982775
gradan-hash <mutukunyaa@gmail.com>
2023-08-09 18:01:01 +0300

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,8 +2,6 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -24,5 +22,32 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             df = pd.read_csv("notebook\data\stud.csv")
-        except:
-            pass
+            logging.info("Read the dataset as df")
+
+            os.makedirs(os.path.dirname(
+                self.ingestion_config.raw_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,
+                      index=False, header=True)
+
+            logging.info("train test split initiated")
+
+            train_set, test_set = train_test_split(
+                df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(
+                self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(
+                self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("ingestion of data completed")
+
+            return (self.ingestion_config.train_data_path, self.ingestion_config.test_data_path)
+
+        except Exception as e:
+            raise CustomException(e, sys.exc_info())
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()  # Create an instance of DataIngestion
+    obj.initiate_data_ingestion()  # Call the method on the instance
