0613b74d22b545cc2a1bd596d5fdc4a2452f5784
gradan-hash <mutukunyaa@gmail.com>
2023-08-10 12:18:05 +0300

    util

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -23,7 +23,7 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
 
-    def get_data_transformer_objective(self):
+    def get_data_transformer_object(self):
         try:
             numercal_columns = ["writing_score", "reading_score"]
             categorical_columns = [
@@ -74,8 +74,48 @@ def initiate_data_transformation(self, train_path, test_path):
         logging.info("Read test data completed")
 
         logging.info("obtaining preprocessor object")
-        
-        
+
+        preprocessing_obj = self.get_data_transformer_object()
+
+        target_column_name = "math_score"
+        numercal_columns = ["writing_score", "reading_score"]
+
+        input_feature_train_df = train_df.drop(
+            columns=[target_column_name], axis=1)
+        target_feature_train_df = train_df[target_column_name]
+
+        input_feature_test_df = test_df.drop(
+            columns=[target_column_name], axis=1)
+        target_feature_test_df = test_df[target_column_name]
+
+        logging.info("appling preprocessor.. ")
+
+        input_feature_train_arr = preprocessing_obj.fit_transform(
+            input_feature_train_df)
+
+        input_feature_test_arr = preprocessing_obj.transform(
+            input_feature_test_df)
+
+        train_arr = np.c_[
+            input_feature_train_arr, np.array(target_feature_train_df)
+
+        ]
+
+        test_arr = np.c_[
+            input_feature_test_arr, np.array(target_feature_test_df)
+
+        ]
+
+        logging.inf("saved preprocessing object")
+
+        save_object(
+            self.data_transformation_config.preprocessor_obj_file_path,
+            obj=preprocessing_obj
+        )
+
+        return (
+            train_arr, test_arr, self.data_transformation_config.preprocessor_obj_file_path
+        )
 
     except:
         pass
