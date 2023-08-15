2010fc05637baf15149a3fcafd718eeeaa5ec77a
gradan-hash <mutukunyaa@gmail.com>
2023-08-15 22:17:02 +0300

    artfacts

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from dataclass import dataclass
+from dataclasses import dataclass
+
 
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (AdaBoostRegressor,
@@ -10,7 +11,9 @@ from sklearn.ensemble import (AdaBoostRegressor,
 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-from sklearn.neighbors import KnearestNeighborsRegressor
+
+from sklearn.neighbors import KNeighborsRegressor
+
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
@@ -29,37 +32,51 @@ class ModelTrainer:
     def __init__(self,):
         self.model_trainer_config = ModelTrainerConfig()
 
-        def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
-            try:
-                logging.info("splitting trainning nd test input data")
-                x_train, y_train, x_test, y_test = (
-                    train_array[:, :-1],
-                    train_array[:, -1],
-                    test_array[:, : -1],
-                    test_array[:, -1],
-                )
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("splitting trainning nd test input data")
+            x_train, y_train, x_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, : -1],
+                test_array[:, -1],
+            )
 
-                models = {
-                    "Random forest": RandomForestRegressor(),
-                    "Decision Tree": DecisionTreeRegressor(),
-                    "Gradient Boosting": GradientBoostingRegressor(),
-                    "Linear Regression": LinearRegressionRegressor(),
-                    "K-NearestNeighbors": KNearestNeighborsRegressor(),
-                    "XGBBclassifier": XGBuilderRegressor(),
-                    "Catboosting Classifier ": CatBoostRegressor(verbose=False),
-                    "AdaBoost classifier ": AdaBoostRegressor()
-                }
-                model_report: dict = evaluate_model(
-                    x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models)
+            models = {
+                "Random forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "K-NearestNeighbors": KNeighborsRegressor(),
+                "XGBBclassifier": XGBRegressor(),
+                "Catboosting Classifier ": CatBoostRegressor(verbose=False),
+                "AdaBoost classifier ": AdaBoostRegressor()
+            }
+            model_report: dict = evaluate_model(
+                x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test, models=models)
 
-                # get the best model score from dict
-                best_model_score = max(sorted(model_report.values()))
+            # get the best model score from dict
+            best_model_score = max(sorted(model_report.values()))
 
-                # to get best model name from dict
+            # to get best model name from dict
 
-                best_model_name = list(model_report.keys())[
-                    list(model_report.values()).index(best_model_score)
-                ]
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
 
-            except Exception as e:
-                raise CustomException(e, sys.exc_info())
+            if best_model_score < 0.60:
+                raise CustomException("no best model found")
+            logging.info(f"best found model training testing..")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(x_test)
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e, sys.exc_info())
--- src/utils.py
+++ src/utils.py
@@ -22,26 +22,25 @@ def save_object(file_path, obj):
         raise CustomException(e, sys.exc_info)
 
 
-def evaluate_model(x_train, y_train,x_test,y_test, models):
+def evaluate_model(x_train, y_train, x_test, y_test, models):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
 
-            models.fit(x_train, y_train)  # traini the miodel
-            
-            y-train_pred =model.predict(x_train)
+            model.fit(x_train, y_train)  # traini the miodel
+
+            y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
-            
-            train_model_score =r2_score(y_train, y_train_pred)
-            
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_scores = r2_score(y_test, y_test_pred)
-            
-            report[list(models.keys())[i]] =test_model_score
+
+            report[list(models.keys())[i]] = test_model_scores
 
             return report
-            
 
     except Exception as e:
-      raise CustomException(e, sys.exc_info())
+        raise CustomException(e, sys.exc_info())
