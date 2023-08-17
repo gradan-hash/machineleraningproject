1b349512489418e70c0739febff75b5d1d44aff4
gradan-hash <mutukunyaa@gmail.com>
2023-08-17 22:35:48 +0300

    render_templates

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import numpy as numpy
 import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+
+app = application
+
+# create route for homepage
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/prediction", methods=["POST,GET"])
+def predict_datapoint():
+    if request.method == "GET":
+        return render_template("home.html")
+      
+    else:
+      pass
